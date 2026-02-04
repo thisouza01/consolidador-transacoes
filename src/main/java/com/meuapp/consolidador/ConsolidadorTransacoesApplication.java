2ea567d0360095b52398dcce75d06288c6694145
thisouza01 <tihsouza01@gmail.com>
2026-02-04 00:28:34 -0300

    Add: Criando template, utilizando Spring Initializer e configurando properties.

---
+++ src/main/java/com/meuapp/consolidador/ConsolidadorTransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.meuapp.consolidador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolidadorTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsolidadorTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuapp/consolidador/ConsolidadorTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuapp.consolidador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolidadorTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
