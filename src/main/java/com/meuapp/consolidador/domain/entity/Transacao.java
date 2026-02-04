608647c5def9525ed8cce99d79d3920352ce4f02
thisouza01 <tihsouza01@gmail.com>
2026-02-04 01:12:45 -0300

    Add: Getters, Setters e o toString para debugar mais fácil.

--- src/main/java/com/meuapp/consolidador/domain/entity/Transacao.java
+++ src/main/java/com/meuapp/consolidador/domain/entity/Transacao.java
@@ -47,4 +47,109 @@ public class Transacao {
     @Column(name = "id_execucao_job")
     private Long idExecucaoJob;
 
+    public Long getIdExecucaoJob() {
+        return idExecucaoJob;
+    }
+
+    public void setIdExecucaoJob(Long idExecucaoJob) {
+        this.idExecucaoJob = idExecucaoJob;
+    }
+
+    public LocalDateTime getDataProcessamento() {
+        return dataProcessamento;
+    }
+
+    public void setDataProcessamento(LocalDateTime dataProcessamento) {
+        this.dataProcessamento = dataProcessamento;
+    }
+
+    public String getHashDuplicada() {
+        return hashDuplicada;
+    }
+
+    public void setHashDuplicada(String hashDuplicada) {
+        this.hashDuplicada = hashDuplicada;
+    }
+
+    public String getMotivoRejeicao() {
+        return motivoRejeicao;
+    }
+
+    public void setMotivoRejeicao(String motivoRejeicao) {
+        this.motivoRejeicao = motivoRejeicao;
+    }
+
+    public StatusTransacao getStatusTransacao() {
+        return statusTransacao;
+    }
+
+    public void setStatusTransacao(StatusTransacao statusTransacao) {
+        this.statusTransacao = statusTransacao;
+    }
+
+    public String getEstabelecimento() {
+        return estabelecimento;
+    }
+
+    public void setEstabelecimento(String estabelecimento) {
+        this.estabelecimento = estabelecimento;
+    }
+
+    public TipoTransacao getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public void setTipoTransacao(TipoTransacao tipoTransacao) {
+        this.tipoTransacao = tipoTransacao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataTransacao() {
+        return dataTransacao;
+    }
+
+    public void setDataTransacao(LocalDate dataTransacao) {
+        this.dataTransacao = dataTransacao;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /* para debug */
+    @Override
+    public String toString() {
+        return "Transacao{" +
+                "id=" + id +
+                ", cpf='" + cpf + '\'' +
+                ", dataTransacao=" + dataTransacao +
+                ", valor=" + valor +
+                ", tipoTransacao=" + tipoTransacao +
+                ", estabelecimento='" + estabelecimento + '\'' +
+                ", statusTransacao=" + statusTransacao +
+                ", motivoRejeicao='" + motivoRejeicao + '\'' +
+                ", hashDuplicada='" + hashDuplicada + '\'' +
+                ", dataProcessamento=" + dataProcessamento +
+                ", idExecucaoJob=" + idExecucaoJob +
+                '}';
+    }
 }
